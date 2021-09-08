e5a01a7f26036bf9e3f5ea10f478d08d2755f7e6
quantumalaviya <45961148+quantumalaviya@users.noreply.github.com>
2021-09-08 22:30:25 +0530

    Final update

--- detect.py
+++ detect.py
@@ -11,16 +11,16 @@ import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
 
-from utils.google_utils import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import (
+from yolor.utils.google_utils import attempt_load
+from yolor.utils.datasets import LoadStreams, LoadImages
+from yolor.utils.general import (
     check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, strip_optimizer)
-from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized
+from yolor.utils.plots import plot_one_box
+from yolor.utils.torch_utils import select_device, load_classifier, time_synchronized
 
-from models.models import *
-from utils.datasets import *
-from utils.general import *
+from yolor.models.models import *
+from yolor.utils.datasets import *
+from yolor.utils.general import *
 
 def load_classes(path):
     # Loads *.names file at 'path'
